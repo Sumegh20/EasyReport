be56e0915a0f28de888124322696303b71b94175
Sumegh20 <sumegh20@gmail.com>
2021-12-10 16:33:18 +0530

    setup updated

--- setup.py
+++ setup.py
@@ -16,17 +16,21 @@ setup(
     keywords=['EDA', 'Exploratory Data Analysis', 'Easy Report', 'Dataset Report'],
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
-    py_modules=['easy_report'],
+    py_module=['easy_report'],
     package_dir={'': 'src'},
     install_requires=[
-        'pandas',
-        'seaborn',
-        'scipy',
-        'matplotlib',
-        'missingno'
+        'pandas==1.1.5',
+        'seaborn==0.11.2',
+        'scipy==1.5.4',
+        'matplotlib==3.3.4',
+        'missingno==0.5.0'
     ]
 )
